dc1039febc1689b6ea6712a06ad542f265b7275d
rayanadir <rayan.dahmena@gmail.com>
2022-05-04 21:56:46 +0200

    adding Footer component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../footer/Footer.scss'
+
+const Footer = () => {
+  const year= new Date().getFullYear()
+  return (
+    <footer className="footer">
+      <p className="footer-text">Copyright {year} Argent Bank</p>
+    </footer>
+  )
+}
+
+export default Footer
