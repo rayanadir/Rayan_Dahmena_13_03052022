79b6f072f90cd413204fd8d7afcb09d3395d13e2
rayanadir <rayan.dahmena@gmail.com>
2022-05-04 21:59:36 +0200

    adding features data

---
+++ src/data/features.js
@@ -0,0 +1,19 @@
+const features = [
+    {
+        src:"./img/icon-chat.png",
+        title:"You are our #1 priority",
+        description:"Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes."
+    },
+    {
+        src:"./img/icon-money.png",
+        title:"More savings means higher rates",
+        description:"The more you save with us, the higher your interest rate will be!",
+    },
+    {
+        src:"./img/icon-security.png",
+        title:"Security you can trust",
+        description:"We use top of the line encryption to make sure your data and money is always safe."
+    }
+]
+
+export default features;
