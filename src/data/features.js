b4582e23d4b8293e5aeee4e1352d853b997ac470
rayanadir <rayan.dahmena@gmail.com>
2022-05-05 20:43:53 +0200

    changing icons path

--- src/data/features.js
+++ src/data/features.js
@@ -1,16 +1,20 @@
+import icon_chat from '../img/icon-chat.png';
+import icon_money from '../img/icon-money.png';
+import icon_security from '../img/icon-security.png';
+
 const features = [
     {
-        src:"./img/icon-chat.png",
+        src:icon_chat,
         title:"You are our #1 priority",
         description:"Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes."
     },
     {
-        src:"./img/icon-money.png",
+        src:icon_money,
         title:"More savings means higher rates",
         description:"The more you save with us, the higher your interest rate will be!",
     },
     {
-        src:"./img/icon-security.png",
+        src:icon_security,
         title:"Security you can trust",
         description:"We use top of the line encryption to make sure your data and money is always safe."
     }
