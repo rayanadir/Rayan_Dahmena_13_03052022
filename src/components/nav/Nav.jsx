8bfd8d4886212936649f6278de234358eab142c9
rayanadir <rayan.dahmena@gmail.com>
2022-05-05 20:41:01 +0200

    changing image path Nav component

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import '../nav/Nav.scss'
 import PropTypes from 'prop-types'
+import argentBankLogo from '../../img/argentBankLogo.png';
 
 const Nav = (props) => {
   return (
       <nav className="main-nav">
-        <a className="main-nav-logo" href="./index.html">
+        <a className="main-nav-logo" href="/">
           <img
             className="main-nav-logo-image"
-            src="./img/argentBankLogo.png"
+            src={argentBankLogo}
             alt="Argent Bank Logo"
           />
           <h1 className="sr-only">Argent Bank</h1>
@@ -16,18 +17,18 @@ const Nav = (props) => {
         {
         props.page === "index" || props.page === "signIn" ?
           <div>
-            <a className="main-nav-item" href="./sign-in.html">
+            <a className="main-nav-item" href="/login">
               <i className="fa fa-user-circle"></i>
               Sign In
             </a>
           </div> 
           : props.type === "user" ? 
           <div>
-            <a className="main-nav-item" href="./user.html">
+            <a className="main-nav-item" href="/profile">
               <i className="fa fa-user-circle"></i>
               Tony
             </a>
-            <a className="main-nav-item" href="./index.html">
+            <a className="main-nav-item" href="/">
               <i className="fa fa-sign-out"></i>
               Sign Out
             </a>
