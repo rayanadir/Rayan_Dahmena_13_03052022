0e7c632a6bfa305350e0e5bae78f9ca95eb24695
rayanadir <rayan.dahmena@gmail.com>
2022-05-05 20:38:46 +0200

    adding propTypes Feature component

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import '../feature/Feature.scss'
+import PropTypes from 'prop-types'
 
 const Feature = (props) => {
   return (
     <div className="feature-item">
-        <img src={props.src} alt="Chat Icon" className="feature-icon" />
-        <h3 className="feature-item-title">{props.title}</h3>
-        <p>{props.description}</p>
+        <img src={props.data.src} alt="Chat Icon" className="feature-icon" />
+        <h3 className="feature-item-title">{props.data.title}</h3>
+        <p>{props.data.description}</p>
     </div>
   )
+}
+
+Feature.propTypes={
+  data:PropTypes.object,
 }
 
 export default Feature
