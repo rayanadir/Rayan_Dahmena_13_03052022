11dae5232fc888eba841886bc2cb8dbb15452bac
rayanadir <rayan.dahmena@gmail.com>
2022-05-05 20:39:46 +0200

    fix attributes Form component

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -5,19 +5,19 @@ const Form = () => {
   return (
     <form>
           <div className="input-wrapper">
-            <label for="username">Username</label>
+            <label htmlFor="username">Username</label>
             <input type="text" id="username" />
           </div>
           <div className="input-wrapper">
-            <label for="password">Password</label>
+            <label htmlFor="password">Password</label>
             <input type="password" id="password" />
           </div>
           <div className="input-remember">
             <input type="checkbox" id="remember-me" />
-            <label for="remember-me">Remember me</label>
+            <label htmlFor="remember-me">Remember me</label>
           </div>
           {/* PLACEHOLDER DUE TO STATIC SITE */} 
-          <a href="./user.html" className="sign-in-button">Sign In</a>
+          <a href="/profile" className="sign-in-button">Sign In</a>
           {/*SHOULD BE THE BUTTON BELOW */}
           {/*<button className="sign-in-button">Sign In</button>*/}
         </form>
