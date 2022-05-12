3d8fb14979ba382c76cf82848e3c65299ce8ad3c
rayanadir <rayan.dahmena@gmail.com>
2022-05-13 00:54:37 +0200

    update User view component

--- src/views/User/User.jsx
+++ src/views/User/User.jsx
@@ -4,11 +4,12 @@ import Header from '../../components/header/Header';
 import Account from '../../components/account/Account';
 
 const User = () => {
+  document.title="Argent Bank - User Page"
   return (
     <>
       <Nav page="user"/>
       <main className='main bg-dark'>
-        <Header name='Tony Jarvis'/>
+        <Header />
         <h2 className="sr-only">Accounts</h2>
         <Account title='Argent Bank Checking (x8349)' amount='$2,082.79' description='Available Balance'/>
         <Account title='Argent Bank Savings (x6712)' amount='$10,928.42' description='Available Balance'/>
