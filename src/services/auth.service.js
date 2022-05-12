d9fd2ad5199c7b3670e4db813de586e1727b8c45
rayanadir <rayan.dahmena@gmail.com>
2022-05-13 00:51:01 +0200

    auth function and get user profile

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { loginFail, loginSuccess } from "../slices/loginSlice";
+import { userFail, userSuccess } from "../slices/userSlice";
 
 const BASE_URL="http://localhost:3001/api/v1";
 
@@ -9,18 +11,25 @@ const login =  (email, password) =>  (dispatch) => {
             sessionStorage.setItem("user", JSON.stringify(response.data));
         }
         console.log("Authentification réussie : ", response.data);
-        dispatch({
-            type:"LOGIN_SUCCESS",
-            payload:response.data
-        })
+        dispatch(userProfile(response.data.body.token))
+        dispatch(loginSuccess(response.data))
         return response.data;
     })
-    .catch((err)=>{
+    .catch((err) =>{
         console.log("Échec de l'authentification : ", err.response.data);
-        dispatch({
-            type:"LOGIN_SUCCESS",
-            payload:err.response.data
-        })
+        dispatch(loginFail(err.response.data.message))
+    })
+}
+
+const userProfile = (token) => (dispatch) => {
+    axios.post(BASE_URL + "/user/profile", {token}, { headers: {"Authorization" : `Bearer ${token}`} })
+    .then((response)=>{
+        console.log("Informations utilisateurs : ", response.data);
+        dispatch(userSuccess(response.data))
+    })
+    .catch((err) => {
+        console.log("Échec de récupération des informations utilisateurs : ", err);
+        dispatch(userFail(err.response))
     })
 }
 
@@ -28,6 +37,6 @@ const logout = () => {
     sessionStorage.removeItem("user");
 }
 
-const auth_service={ login, logout }
+const auth_service={ login, logout, userProfile }
 
 export default auth_service
