6554dafc5a30c806eaed124e2c37fa5a5c1a2adb
rayanadir <rayan.dahmena@gmail.com>
2022-05-16 23:12:42 +0200

    update User view component

--- src/views/User/User.jsx
+++ src/views/User/User.jsx
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import Nav from '../../components/nav/Nav';
 import Header from '../../components/header/Header';
 import Account from '../../components/account/Account';
+import auth_service from '../../services/auth.service';
 
 const User = () => {
   document.title="Argent Bank - User Page";
+  const token= useSelector((state)=> state.login.token);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  dispatch(auth_service.userProfile(token)); 
+  
+  useEffect(()=>{
+    if(token === null){
+      navigate('/')
+    }
+  },[token, navigate])
+
   return (
     <>
       <Nav />
-      <main className='main bg-dark'>
+      {
+        token !==null ?
+        <main className='main bg-dark'>
         <Header />
         <h2 className="sr-only">Accounts</h2>
         <Account title='Argent Bank Checking (x8349)' amount='$2,082.79' description='Available Balance'/>
         <Account title='Argent Bank Savings (x6712)' amount='$10,928.42' description='Available Balance'/>
         <Account title='Argent Bank Credit Card (x8349)' amount='$184.30' description='Current Balance'/>
       </main>
+      :<main className='main bg-dark'></main>
+      }
     </>
   )
 }
