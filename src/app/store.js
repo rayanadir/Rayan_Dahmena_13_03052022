2fecebb329fa0bfbe32c72f54ea6a2cfe3776d1d
rayanadir <rayan.dahmena@gmail.com>
2022-05-16 23:06:05 +0200

    add redux-persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,40 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { loginReducer } from "../slices/loginSlice";
 import { userReducer } from "../slices/userSlice";
+import {
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+  } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-export default configureStore({
-    reducer:{
-        login: loginReducer,
-        user: userReducer
-    },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck:false}),
-})
+import { combineReducers } from "@reduxjs/toolkit";
+
+const rootReducer = combineReducers({
+    login: loginReducer,
+    user: userReducer
+}) 
+
+const persistConfig = {
+ key: 'root',
+ version: 1,
+ storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+      }),
+  })
+  
