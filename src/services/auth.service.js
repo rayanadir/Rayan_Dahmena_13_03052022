25c4650c494b9477f796e60058875a45d33a432a
rayanadir <rayan.dahmena@gmail.com>
2022-05-16 23:10:39 +0200

    update logout function

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,22 +1,21 @@
 import axios from "axios";
-import { loginFail, loginSuccess } from "../slices/loginSlice";
+import { loginFail, loginSuccess, logoutSuccess } from "../slices/loginSlice";
 import { userFail, userLogout, userSuccess, userUpdateFail, userUpdateSuccess } from "../slices/userSlice";
 
 const BASE_URL = "http://localhost:3001/api/v1";
 
-const login = (email, password) => (dispatch) => {
+const login = (email, password, rememberMe) => (dispatch) => {
     axios.post(BASE_URL + "/user/login", { email, password })
         .then((response) => {
-            if (response.data.body.token) {
-                sessionStorage.setItem("user", JSON.stringify(response.data));
+            if (rememberMe) {
+                localStorage.setItem("token", JSON.stringify(response.data.body.token));
+            }else{
+                sessionStorage.setItem("token", JSON.stringify(response.data.body.token));
             }
-            console.log("Authentification réussie : ", response.data);
-            dispatch(userProfile(response.data.body.token))
             dispatch(loginSuccess(response.data))
             return response.data;
         })
         .catch((err) => {
-            console.log("Échec de l'authentification : ", err.response.data);
             dispatch(loginFail(err.response.data.message))
         })
 }
@@ -24,11 +23,9 @@ const login = (email, password) => (dispatch) => {
 const userProfile = (token) => (dispatch) => {
     axios.post(BASE_URL + "/user/profile", { token }, { headers: { "Authorization": `Bearer ${token}` } })
         .then((response) => {
-            console.log("Informations utilisateurs : ", response.data);
             dispatch(userSuccess(response.data))
         })
         .catch((err) => {
-            console.log("Échec de récupération des informations utilisateurs : ", err);
             dispatch(userFail(err.response))
         })
 }
@@ -47,9 +44,10 @@ const updateProfile = (firstName, lastName, token) => (dispatch) => {
 }
 
 const logout = () => (dispatch) => {
+    sessionStorage.clear();
+    localStorage.clear();
     dispatch(userLogout());
-    dispatch(logout());
-    sessionStorage.removeItem("user");
+    dispatch(logoutSuccess());
 }
 
 const auth_service = { login, logout, userProfile, updateProfile }
