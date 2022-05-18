ea275f0b97eae0d2ada49fab72d4415b063f7b81
rayanadir <rayan.dahmena@gmail.com>
2022-05-18 13:03:31 +0200

    add comments

--- src/app/store.js
+++ src/app/store.js
@@ -11,23 +11,33 @@ import {
     REGISTER,
   } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-
 import { combineReducers } from "@reduxjs/toolkit";
 
+/**
+ * Combines reducers (login and user)
+ */
 const rootReducer = combineReducers({
     login: loginReducer,
     user: userReducer
 }) 
 
+/**
+ * Creates persist configuration
+ */
 const persistConfig = {
  key: 'root',
  version: 1,
  storage,
 }
 
+/**
+ * Creates persistedReducer
+ */
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-
+/**
+ * Configures store
+ */
 export const store = configureStore({
     reducer: persistedReducer,
     middleware: (getDefaultMiddleware) =>
