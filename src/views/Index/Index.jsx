8e2ff71dab8a8f9e88ba0540381d2dc4ce7bda1f
rayanadir <rayan.dahmena@gmail.com>
2022-05-24 19:49:31 +0200

    user stays in index page after logo click

--- src/views/Index/Index.jsx
+++ src/views/Index/Index.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Hero from '../../components/hero/Hero'
 import Nav from '../../components/nav/Nav'
 import Features from '../../containers/features/Features'
+import { useNavigate } from 'react-router-dom';
+import { useSelector  } from 'react-redux';
 
 /**
  * Creates Index page component
@@ -9,6 +11,16 @@ import Features from '../../containers/features/Features'
  */
 const Index = () => {
   document.title="Argent Bank - Home Page";
+  const navigate = useNavigate()
+  const token= useSelector((state)=> state.login.token)
+  const logoClick= useSelector((state) =>  state.login.logoClick)
+  useEffect(()=>{
+    if((token !== null || localStorage.getItem('token') !== null) && logoClick!==true){
+      navigate('/profile')
+    }else if (logoClick===true){
+      navigate('/')
+    }
+  },[token, navigate,logoClick])
   return (
     <>
         <Nav />
